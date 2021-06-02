ba7ec330f0e23b34a02fdf4571c5126612756f5e
Jdan2 <juandortiz.15@gmail.com>
2021-06-02 09:30:57 -0500

    Se mejora la funcionalidad de completado

--- frontEnd/my-app/src/App.js
+++ frontEnd/my-app/src/App.js
@@ -139,8 +139,7 @@ const List = () => {
           return <tr key={todo.id} style={todo.completed ? decorationDone : {}}>
             <td>{todo.id}</td>
             <td>{todo.name}</td>
-            <td>{todo.isCompleted === true ? "SI" : "NO"}</td>
-            
+            <td><input type="checkbox" defaultChecked={todo.completed} onChange={(event) => onChange(event, todo)}></input></td>
             <td><button onClick={() => onDelete(todo.id)}>Eliminar</button></td>
             <td><button onClick={() => onEdit(todo)}>Editar</button></td>
           </tr>
